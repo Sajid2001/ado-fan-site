2e65d41b666dd1adee152163d2ec0b4d481ea641
Sajid2001 <60523377+Sajid2001@users.noreply.github.com>
2023-05-07 21:23:34 -0400

    small changes to style

--- src/Components/Music.js
+++ src/Components/Music.js
@@ -7,7 +7,7 @@ const Music = () => {
   return (
     <div id='music' className='my-8'>
         <div className='relative'>
-            <img src={adomusic} alt='No image loaded' />
+            <img className='opacity-70' src={adomusic} alt='No image loaded' />
             <h1 className='absolute text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-5xl lg:text-9xl font-display font-bold p-4'>Music</h1>
         </div>
         <div className='flex flex-col md:flex-row justify-center flex-wrap my-8'>
