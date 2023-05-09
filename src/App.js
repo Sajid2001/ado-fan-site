70ec031b7e9ba60558699bcfc95d64b4a279b8b7
Sajid2001 <60523377+Sajid2001@users.noreply.github.com>
2023-05-09 16:10:35 -0400

    extra changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import Banner from "./Components/Banner";
 import Bio from "./Components/Bio";
 import Footer from "./Components/Footer";
 import Music from "./Components/Music";
-import Navbar from "./Components/Navbar";
 
 function App() {
   return (
