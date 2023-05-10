ecbcb637e1516e404c8f82ae38f83cc0e2f10d45
Sajid2001 <60523377+Sajid2001@users.noreply.github.com>
2023-05-09 23:47:22 -0400

    added volume bar

--- src/Components/AudioPlayer.js
+++ src/Components/AudioPlayer.js
@@ -3,6 +3,9 @@ import {GrRotateLeft} from 'react-icons/gr'
 import {GrRotateRight} from 'react-icons/gr'
 import {FaPlayCircle} from 'react-icons/fa'
 import {FaPauseCircle} from 'react-icons/fa'
+import {BsFillVolumeUpFill} from 'react-icons/bs'
+import {BsFillVolumeOffFill} from 'react-icons/bs'
+import {BsVolumeDownFill} from 'react-icons/bs'
 
 
 const AudioPlayer = ({song, image, title}) => {
@@ -15,12 +18,16 @@ const AudioPlayer = ({song, image, title}) => {
   const audioPlayer = useRef();
   const progressBar = useRef(); //reference to progress bar
   const animationRef = useRef(); //reference animation
+  const volumeBar = useRef(1);
 
 
   useEffect(() => {
     const seconds = Math.floor(audioPlayer.current.duration);
     setDuration(seconds);
     progressBar.current.max = seconds;
+    if(volumeBar.current.value == null){
+      volumeBar.current.value = 1;
+    }
   }, [audioPlayer?.current?.loadedmetadata, audioPlayer?.current?.readyState])
 
   const calculateTime = (secs) => {
@@ -55,6 +62,10 @@ const AudioPlayer = ({song, image, title}) => {
     ChangePlayerCurrentTime();
   }
 
+  const changeVolume = () => {
+    audioPlayer.current.volume = volumeBar.current.value;
+  }
+
   const ChangePlayerCurrentTime = () => {
     progressBar.current.style.setProperty('--get-before-width',`${progressBar.current.value/duration * 100}%`);
     setCurrentTime(progressBar.current.value);
@@ -72,7 +83,7 @@ const AudioPlayer = ({song, image, title}) => {
 
   return (
     <div className = "bg-white justify-items-center w-2/3 lg:w-1/4 lg:m-5 my-4 rounded-xl overflow-hidden">
-        <img src= {image} alt="No image read"/>
+        <img src= {image} alt="Nothing read"/>
         <audio ref={audioPlayer} src = {song} preload = "metadata"></audio>
         <div className="flex justify-center -translate-y-7">
           <button onClick={backTen} className = "bg-white border-0 text-2xl rounded-l-lg px-2 pt-1"><GrRotateLeft /></button>
@@ -87,8 +98,16 @@ const AudioPlayer = ({song, image, title}) => {
           <div className='px-2 font-bold font-instrument'>{(duration && !isNaN(duration)) && calculateTime(duration-currentTime)}</div>
         </div>
         
-        <div className='px-1 -translate-y-6'>
+        <div className='p-2 mx-2 -translate-y-6 '>
           <input defaultValue="0" ref={progressBar} className="relative appearance-none outline-none w-full bg-black h-0.5 rounded-lg slider-thumb" onChange={changeRange} type = "range" />
+          <div className = "text-2xl translate-x-10 ">
+            <input id="default-range" ref={volumeBar} defaultValue = "1" type="range" min="0" max="1" step = "0.01" className="appearance-none outline-none w-3/4 bg-black h-0.5 rounded-lg slider-thumb"  onChange={changeVolume}/>
+              {volumeBar.current.value > 0 
+                ? volumeBar.current.value > 0.4
+                  ?<BsFillVolumeUpFill/>
+                    :<BsVolumeDownFill/>
+                : <BsFillVolumeOffFill/>}
+          </div>
         </div>
         
     </div>
