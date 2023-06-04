83565efd48ed8ee65c284792d5dbf2f9899e2183
Sajid2001 <60523377+Sajid2001@users.noreply.github.com>
2023-06-03 21:16:36 -0400

    fixed social links

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -16,7 +16,11 @@ const Footer = () => {
     
                         <ul className="space-y-8 list-none">
                             <li>
-                                <a href="https://twitter.com/ado1024imokenp" className="flex items-center space-x-3 hover:text-sky-400 transition">
+                                <a 
+                                href="https://twitter.com/ado1024imokenp" 
+                                className="flex items-center space-x-3 hover:text-sky-400 transition"
+                                target = '_blank' 
+                                >
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="w-5" viewBox="0 0 16 16">
                                       <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
                                     </svg>
@@ -24,7 +28,10 @@ const Footer = () => {
                                 </a>
                             </li>
                             <li>
-                                <a href="https://www.youtube.com/@Ado1024" className="flex items-center space-x-3 hover:text-red-500 transition">
+                                <a href="https://www.youtube.com/@Ado1024" 
+                                target = '_blank'  
+                                className="flex items-center space-x-3 hover:text-red-500 transition"
+                                >
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="w-5" viewBox="0 0 16 16">
                                       <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.007 2.007 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.007 2.007 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31.4 31.4 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.007 2.007 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A99.788 99.788 0 0 1 7.858 2h.193zM6.4 5.209v4.818l4.157-2.408L6.4 5.209z"/>
                                     </svg>
@@ -32,7 +39,11 @@ const Footer = () => {
                                 </a>
                             </li>
                             <li>
-                                <a href="https://open.spotify.com/artist/6mEQK9m2krja6X1cfsAjfl" className="flex items-center space-x-3 hover:text-green-400 transition">
+                                <a 
+                                href="https://open.spotify.com/artist/6mEQK9m2krja6X1cfsAjfl" 
+                                target = '_blank' 
+                                className="flex items-center space-x-3 hover:text-green-400 transition"
+                                >
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="w-5" viewBox="0 0 16 16"> <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.669 11.538a.498.498 0 0 1-.686.165c-1.879-1.147-4.243-1.407-7.028-.77a.499.499 0 0 1-.222-.973c3.048-.696 5.662-.397 7.77.892a.5.5 0 0 1 .166.686zm.979-2.178a.624.624 0 0 1-.858.205c-2.15-1.321-5.428-1.704-7.972-.932a.625.625 0 0 1-.362-1.194c2.905-.881 6.517-.454 8.986 1.063a.624.624 0 0 1 .206.858zm.084-2.268C10.154 5.56 5.9 5.419 3.438 6.166a.748.748 0 1 1-.434-1.432c2.825-.857 7.523-.692 10.492 1.07a.747.747 0 1 1-.764 1.288z"/> </svg>
                                     <span>Spotify</span>
                                 </a>
@@ -40,7 +51,11 @@ const Footer = () => {
                             
                         </ul>
                     </div>
-                    <div className="w-10/12 m-auto  mt-16 space-y-6 text-center sm:text-left sm:w-5/12 sm:mt-auto">
+                    <div 
+                    className="w-10/12 m-auto  
+                    mt-16 space-y-6 text-center sm:text-left 
+                    sm:w-5/12 sm:mt-auto"
+                    >
                         <span className="block text-gray-300">This is a fanmade website. </span>
                         <span className="block text-gray-300">The songs and images used in this website do not belong to me and instead belong to Ado.</span>
                         
@@ -49,7 +64,14 @@ const Footer = () => {
             </div>
         </div>
         <div aria-hidden="true" className="absolute h-full inset-0 flex items-center">
-            <div aria-hidden="true" className="bg-layers bg-scale w-56 h-56 m-auto blur-xl bg-gradient-to-r from-cyan-400 via-blue-600 to-purple-800 rounded-full md:w-[30rem] md:h-[30rem] md:blur-3xl"></div>
+            <div 
+            aria-hidden="true" 
+            className="bg-layers bg-scale w-56 h-56 m-auto 
+            blur-xl bg-gradient-to-r from-cyan-400 via-blue-600 to-purple-800 
+            rounded-full md:w-[30rem] md:h-[30rem] md:blur-3xl"
+            >
+
+            </div>
         </div>
         <div aria-hidden="true" className="absolute inset-0 w-full h-full bg-[#020314] opacity-80"></div>
     </footer>
